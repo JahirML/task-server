76b8d1dde61e943ed8641ee27508eafada4dd2cf
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-09 18:45:23 -0600

    add morgan

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,14 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 import router from "./routes/ProjectRoutes";
 import { corosConfig } from "./config/cors";
+import morgan from "morgan";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+// login
+app.use(morgan("dev"));
 app.use(cors(corosConfig));
 //! Routes
 // app.use('/api/auth',router);
