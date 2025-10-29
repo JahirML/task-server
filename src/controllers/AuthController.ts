0394f1bc604c156c5731d1c9599f1c4ae5a3cdfe
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-28 20:50:27 -0600

    implement hash of a passwprd

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,15 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
-
+import bcrypt from "bcrypt";
+import { hashPassword } from "../utils/auth";
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { password } = req.body;
       const user = new User(req.body);
+
+      // hash password
+      user.password = await hashPassword(password);
       await user.save();
       res.send("Cuenta creada, revisa tu email para confirmarla");
     } catch (error) {
