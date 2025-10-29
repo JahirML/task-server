79a5e35b0394d3a3d0e6022274bfe26b7001c98c
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-28 21:01:45 -0600

    prevent user exists in the database

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,18 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
-import bcrypt from "bcrypt";
 import { hashPassword } from "../utils/auth";
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
-      const { password } = req.body;
+      const { password, email } = req.body;
+
+      // Prevenir email dulicados
+      const userExist = await User.findOne({ email });
+
+      if (userExist) {
+        const error = new Error("Este usuario ya esta registrado");
+        return res.status(409).json({ error: error.message });
+      }
       const user = new User(req.body);
 
       // hash password
