f06aa0c1b00f6bc81a373bae060b57006d3cb12e
Jahir Mora <jahiragustin1234@gmail.com>
2025-10-29 22:07:41 -0600

    add nodemailer

---
+++ src/config/nodemailer.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+
+dotenv.config();
+const config = () => {
+  return {
+    host: process.env.SMTP_HOST,
+    port: +process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  };
+};
+// Looking to send emails in production? Check out our Email API/SMTP product!
+export const transporter = nodemailer.createTransport(config());
