e229503e0ac9d9a356d62fe352c98a2797f7ca54
Jahir Mora <jahiragustin1234@gmail.com>
2025-11-04 18:08:01 -0600

    ADD JWT

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,6 +4,7 @@ import { checkPassword, hashPassword } from "../utils/auth";
 import Token from "../models/Token";
 import { generateToken } from "../utils/token";
 import { AuthEmail } from "../emails/AuthEmail";
+import { generateJWT } from "../utils/jwt";
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
@@ -89,7 +90,8 @@ export class AuthController {
         const error = new Error("La contraseña es incorrecta");
         return res.status(404).json({ error: error.message });
       }
-      res.send("Autenticado");
+      const token = generateJWT({ id: user.id });
+      res.send(token);
     } catch (error) {
       res.status(500).json({ error: "Hubo un error" });
     }
