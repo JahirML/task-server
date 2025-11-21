706dc51276ad847c07eb27733d7d9ed5dca1bb49
Jahir Mora <jahiragustin1234@gmail.com>
2025-11-21 16:49:12 -0600

    delete notes when a task is deteled

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
+import Note from "./Note";
 
 const taskStatus = {
   PENDIG: "pending",
@@ -67,6 +68,18 @@ export const TaskSchema: Schema = new Schema(
 
   { timestamps: true }
 );
+// Middleware
+TaskSchema.pre(
+  "deleteOne",
+  {
+    document: true,
+  },
+  async function () {
+    const taskId = this._id;
+    if (!taskId) return;
 
+    await Note.deleteMany({ task: taskId });
+  }
+);
 const Task = mongoose.model<ITask>("Task", TaskSchema);
 export default Task;
