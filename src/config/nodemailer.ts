413483d6fc41bdffc2caecb75ca105de0cfd4d95
Jahir Mora <jahiragustin1234@gmail.com>
2025-11-28 20:27:04 -0600

    configure node mailer

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -6,11 +6,14 @@ const config = () => {
   return {
     host: process.env.SMTP_HOST,
     port: +process.env.SMTP_PORT,
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
+    tls: {
+      rejectUnauthorized: false,
+    },
   };
 };
-// Looking to send emails in production? Check out our Email API/SMTP product!
 export const transporter = nodemailer.createTransport(config());
